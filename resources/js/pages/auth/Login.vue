48a401abb1e1bf505f713a2043ff39c04e2a652a
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-03-21 01:00:23 +0800

    you can login using name or email

--- resources/js/pages/auth/Login.vue
+++ resources/js/pages/auth/Login.vue
@@ -15,7 +15,7 @@ defineProps<{
 }>();
 
 const form = useForm({
-    email: '',
+    login: '',
     password: '',
     remember: false,
 });
@@ -28,7 +28,7 @@ const submit = () => {
 </script>
 
 <template>
-    <AuthBase title="Log in to your account" description="Enter your email and password below to log in">
+    <AuthBase title="Log in to your account" description="Enter your login and password below to log in">
         <Head title="Log in" />
 
         <div v-if="status" class="mb-4 text-center text-sm font-medium text-green-600">
@@ -38,18 +38,18 @@ const submit = () => {
         <form @submit.prevent="submit" class="flex flex-col gap-6">
             <div class="grid gap-6">
                 <div class="grid gap-2">
-                    <Label for="email">Email address</Label>
+                    <Label for="login">login address</Label>
                     <Input
-                        id="email"
-                        type="email"
+                        id="login"
+                        type="login"
                         required
                         autofocus
                         :tabindex="1"
-                        autocomplete="email"
-                        v-model="form.email"
-                        placeholder="email@example.com"
+                        autocomplete="login"
+                        v-model="form.login"
+                        placeholder="login@example.com"
                     />
-                    <InputError :message="form.errors.email" />
+                    <InputError :message="form.errors.login" />
                 </div>
 
                 <div class="grid gap-2">
