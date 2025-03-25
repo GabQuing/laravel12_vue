ce2eb4b7e34ef6d190537206ff45145d9253d083
Camerone <130955631+GabQuing@users.noreply.github.com>
2025-03-26 06:19:45 +0800

    added edit front end

---
+++ resources/js/pages/EditProduct.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import ProductTable from '@/components/ProductTable.vue';
+import CreateNewProduct from '@/components/CreateNewProduct.vue';
+
+
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Create Product',
+        href: '/product/create',
+    },
+];
+</script>
+
+<template>
+    <Head title="Create Product" />
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex h-full flex-1 flex-col gap-4 rounded-xl py-4 ">
+            <div class="relative min-h-[100vh] flex-1 rounded-xl border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+            <EditNewProduct/>
+            </div>
+        </div>
+    </AppLayout>
+</template>
